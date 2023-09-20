ba60dbbea724407e6bcdac2140a1a5b33ab2eff7
chepetinaki <chepetinaki@gmail.com>
2023-09-20 19:21:52 +0530

    src/test/java/com/example/hitman/HitManApplicationTests.java

---
+++ src/test/java/com/example/hitman/HitManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hitman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
