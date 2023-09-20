67aa2e93da862b3d82b02ade928bc9e6ed7c3f9e
chepetinaki <chepetinaki@gmail.com>
2023-09-20 19:21:07 +0530

    src/main/java/com/example/hitman/HitManApplication.java

---
+++ src/main/java/com/example/hitman/HitManApplication.java
@@ -0,0 +1,13 @@
+package com.example.hitman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitManApplication.class, args);
+	}
+
+}
